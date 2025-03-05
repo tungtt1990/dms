b28b7c1962d482b188afc6e6379febd484e03c01
Tung Tran <tungtt.group1@gmail.com>
2025-03-05 09:42:30 +0700

    update connection string

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // VD: postgresql://user:pass@host:port/dbname
-  ssl: { rejectUnauthorized: false } // Required for Supabase
+  ssl: false
 });
 
 module.exports = pool;
